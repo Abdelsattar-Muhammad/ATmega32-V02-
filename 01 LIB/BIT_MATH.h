6a97b5476db1497814a46826930d26aded3624d4
Abdelsattar Muhammad <130950623+Abdelsattar-Muhammad@users.noreply.github.com>
2024-03-07 11:39:35 +0200

    Add files via upload

---
+++ 01 LIB/BIT_MATH.h
@@ -0,0 +1,19 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 20 Feb 2024             */
+/* Version : V02		  			 */
+/* MCU 	   : ATMEGA32                */
+/*************************************/
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+#define SET_BIT(Reg,BitN)        Reg |= (1U<<BitN)
+
+#define CLR_BIT(Reg,BitN)        Reg &= (~(1U<<BitN))
+
+#define TOGGLE_BIT(Reg,BitN)     Reg ^= (1U<<BitN)
+
+#define GET_BIT(Reg,BitN)       ((Reg>>BitN)&1U)
+
+#endif /* BIT_MATH_H_ */
---
+++ 01 LIB/DATA_TYPES.h
@@ -0,0 +1,23 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 22 June 2023            */
+/* Version : V01					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
+
+
+#ifndef DATA_TYPES_H_
+#define DATA_TYPES_H_
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed long sint32;
+typedef signed long long sint64;
+typedef float f32;
+typedef double f64;
+
+#endif /* DATA_TYPES_H_ */
